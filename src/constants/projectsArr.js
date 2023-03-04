35dc1a1b04576023e8a59e5a0c35711d96b5c617
Valery-Lytvyn <lytvynvalery@gmail.com>
2023-03-04 12:59:23 +0200

    added media query to css

--- src/constants/projectsArr.js
+++ src/constants/projectsArr.js
@@ -11,28 +11,28 @@ export const projectsArr =
          link: "https://valery-lytvyn.github.io/Todo_App",
          linkText: "github.io/Todo-App",
          githubLink: "https://github.com/Valery-Lytvyn/Todo_App",
-         githubLinkText: "github.com/Valery-Lytvyn/Todo-App",
+         githubLinkText: "github.com/Todo-App",
       },
       {
          image: weatherForecastImg,
          link: "https://valery-lytvyn.github.io/weather_forecast/",
-         linkText: "github.io/weather-forecast/",
+         linkText: "github.io/weather/",
          githubLink: "https://github.com/Valery-Lytvyn/weather_forecast",
-         githubLinkText: "github.com/Valery-Lytvyn/weather-forecast",
+         githubLinkText: "github.com/weather",
       },
       {
          image: landingImg,
          link: "https://valery-lytvyn.github.io/LANDING_PAGE_MONTICELLO",
          linkText: "github.io/LANDING-PAGE",
          githubLink: "https://github.com/Valery-Lytvyn/LANDING_PAGE_MONTICELLO",
-         githubLinkText: "github.com/Valery-Lytvyn/LANDING-PAGE-MONTICELLO",
+         githubLinkText: "github.com/LANDING-PAGE",
       },
       {
          image: searchMoviesImg,
          link: "https://valery-lytvyn.github.io/search_movies",
          linkText: "github.io/search-movies",
          githubLink: "https://github.com/Valery-Lytvyn/search_movies",
-         githubLinkText: "github.com/Valery-Lytvyn/search-movies",
+         githubLinkText: "github.com/search-movies",
       },
    ];
 
